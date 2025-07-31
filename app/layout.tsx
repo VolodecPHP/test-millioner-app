881f1dbcb0c3a13a9d553409b00691b7a70258ce
Volodymyr Muryn <v.muryn@autodoc.eu>
2025-07-31 22:41:31 +0300

    test: test thumbnail

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,18 @@ export const metadata: Metadata = {
   title: 'Who Wants to Be a Millionaire?',
   description:
     'Test your knowledge at free online game based on the famous TV quiz show , give the correct answers, and win some amazing prizes!',
+  openGraph: {
+    title: 'My Page Title',
+    description: 'Page description here',
+    images: [
+      {
+        url: '/start-desktop-v1.png',
+        width: 1200,
+        height: 630,
+        alt: 'My Thumbnail',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
